63ce54be99d9bc0b7c7edec9054b3502a36b7821
Yogibabu2 <136948550+Yogibabu2@users.noreply.github.com>
2023-09-29 12:46:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,26 @@
  resource "aws_instance" "production-server" {
- ami = "ami-007855ac798b5175e"
+ ami = "ami-0f5ee92e2d63afc18"
  instance_type = "t2.micro"
- availability_zone = "us-east-1a"
- VPC_security_groups = [aws_security_group.ec2group123.id]
- key_name = "jenkinskey1"
+ key_name = "mynewpair"
+ availability_zone = "ap-south-1a"
+ VPC_security_groups = ["sg-0d7dbd42022748447"]
+ connection {
+type  = "ssh"
+user = "ubuntu"
+private_key =file("./mynewpair.pem")
+host     =self.public_ip
+}
+provisioner "remote-exec" {
+inline = [
+     "echo 'wait to start instance'"]
+}
  tags = {
- name = "ansible_instance"
+ name = "test-server"
  }
  provisioner "remote-exec" {
- inline = [
-     "ansible-playbook bankdeployplaybook.yml"]
+ command = " echo ${aws_instance,test-server,public_ip} > inventory "
+}
+provisioner "remote-exec" {
+     "ansible-playbook deploy.yml"
      }
      }
